4a43071123c855a6234e4c48f7a152caa6add6c3
jchav209 <chavezjulio67@gmail.com>
2023-03-01 04:37:09 +0000

    Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,12 @@ public class Lab {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
 
+    
+    public void throwUncheckedException() throws ArithmeticException {
+        int a;
+        int b;
+        throw new ArithmeticException();
     }
+
 }
